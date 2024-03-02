ccca824439ffe390a66a2a0576ed071d869e8304
klinefelters <stevenjk03@gmail.com>
2024-03-02 12:51:37 -0500

    Added chakra, routers, and general layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,25 @@
 import './App.css'
-function App() {
+import { Routes, Route} from "react-router-dom";
+import { Flex } from '@chakra-ui/react';
 
+// Components
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+
+// Pages
+import Home from './pages/Home';
+
+
+export default function App() {
   return (
-    <>
-    </>
+    <Flex flexDirection="column" minHeight="100vh">
+      <Navbar />
+      <Flex flex="1" direction="column" p="4">
+        <Routes>
+          <Route path="/" element={<Home/>} />
+        </Routes>
+      </Flex>
+      <Footer />
+    </Flex>
   )
 }
-
-export default App
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { BrowserRouter as Router } from "react-router-dom";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  config: {
+    initialColorMode: "dark",
+    useSystemColorMode: false,
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <Router>
+        <App />
+      </Router>
+    </ChakraProvider>
   </React.StrictMode>,
 )
