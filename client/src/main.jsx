aa94d38878aefa65a53fef4c185ba59d4dbf9849
klinefelters <stevenjk03@gmail.com>
2024-03-02 12:53:27 -0500

    Added colors

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,18 @@ import App from './App.jsx'
 import { BrowserRouter as Router } from "react-router-dom";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
+const colors = {
+  brand: {
+    darkblue: '#0A2240',
+    midblue: '#004B98',
+    lightblue: '#3DB5E6',
+    grey: '#C8C8C8',
+    green: '#61BF1A',
+    red: '#E1261C',
+  },
+  colors,
+}
+
 const theme = extendTheme({
   config: {
     initialColorMode: "dark",
