d9ca142f9beac94f3ed892eeb23fc6288ca1a688
yasudat83 <142951380+yasudat83@users.noreply.github.com>
2024-03-02 17:29:46 -0500

    Events rough draft

--- client/src/pages/Events.jsx
+++ client/src/pages/Events.jsx
@@ -1,7 +1,82 @@
+import React, { useState, useEffect } from 'react';
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from '@chakra-ui/react'
+
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from '@chakra-ui/react'
+
+import { Box, useDisclosure, Button, Center } from '@chakra-ui/react';
+import postsData from '../posts.json';
+
 export default function Events() {
+
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [selectedItemIndex, setSelectedItemIndex] = useState(null);
+
+  const handleItemClick = (index) => {
+    setSelectedItemIndex(index);
+    onOpen();
+  }
+
   return (
-    <div>
-      <h1>Welcome to the Events page</h1>
-    </div>
+    <Center>
+        <TableContainer w="50vw">
+        <Table variant='simple' colorScheme='teal'>
+          <Thead>
+            <Tr>
+              <Th textAlign="center">Company</Th>
+              <Th textAlign="center">Event Date</Th>
+              <Th textAlign="center">Event Title</Th>
+              <Th textAlign="center">City</Th>
+              <Th textAlign="center">State</Th>
+            </Tr>
+          </Thead>
+
+          <Tbody>
+          {postsData.map((user, index) => (
+              <Tr key={index} onClick={() => handleItemClick(index)} _hover={{ backgroundColor: 'blue.600' }}>
+                <Td textAlign="center">{user.company.name}</Td>
+                <Td textAlign="center">{user.eventDate}</Td>
+                <Td textAlign="center">{user.title}</Td>
+                <Td textAlign="center">{user.address.city}</Td>
+                <Td textAlign="center">{user.address.state}</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Event Details</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {selectedItemIndex !== null && postsData[selectedItemIndex].description}
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant='ghost'>Sign Up</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </Center>
   )
 }
