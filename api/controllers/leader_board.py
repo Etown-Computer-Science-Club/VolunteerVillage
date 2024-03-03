d6e63d7a371d825c04c03be0eba1563c806dcc43
Ethan Lajeunesse <ethan@lajfam.com>
2024-03-03 01:58:12 -0500

    basic auth

--- api/controllers/leader_board.py
+++ api/controllers/leader_board.py
@@ -2,11 +2,13 @@ from flask import Flask, Blueprint, jsonify
 from database.db import db
 from database.volunteer import Volunteer
 from sqlalchemy import func
+from auth import requires_auth
 
 bp = Blueprint('leader_board', __name__)
 
 
 @bp.route('/leaderboard', methods=['GET'])  # Update the route
+@requires_auth
 def get_leaderboard():
     confirmed_volunteers = db.session.query(
         Volunteer.userId,
