32612a2336434e1065bb549f986b464148cc3da3
Ethan Lajeunesse <ethan@lajfam.com>
2024-03-03 05:21:35 -0500

    Profile page

--- api/main.py
+++ api/main.py
@@ -9,6 +9,7 @@ from controllers.posts import bp as posts_bp
 from controllers.badges import bp as badges_bp
 from controllers.volunteers import bp as volunteers_bp
 from controllers.leader_board import bp as leader_board_bp
+from controllers.profile import bp as profile_bp
 
 load_dotenv()
 
@@ -20,6 +21,7 @@ app.register_blueprint(posts_bp)
 app.register_blueprint(badges_bp)
 app.register_blueprint(volunteers_bp)
 app.register_blueprint(leader_board_bp)
+app.register_blueprint(profile_bp)
 db.init_app(app)
 
 if __name__ == '__main__':
