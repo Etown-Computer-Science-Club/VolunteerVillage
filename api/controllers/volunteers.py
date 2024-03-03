e51964a32d0f882a0b9b7cf479516e806de353e7
Ethan Lajeunesse <ethan@lajfam.com>
2024-03-02 21:59:57 -0500

    Add volunteers endpoint

---
+++ api/controllers/volunteers.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, jsonify, request
+from database.db import db
+from database.volunteer import Volunteer
+
+bp = Blueprint('volunteers', __name__)
+
+
+@bp.route('/volunteers/<int:postId>', methods=['GET'])
+def get_volunteers(postId):
+    volunteers = Volunteer.query.filter_by(postId=postId).all()
+
+    volunteers_data = []
+    for volunteer in volunteers:
+        volunteer_data = {
+            'userId': volunteer.userId,
+            'postId': volunteer.postId,
+            'signedUpAt': volunteer.signedUpAt.isoformat(),
+            'isConfirmed': volunteer.isConfirmed,
+            'confirmedAt': volunteer.confirmedAt.isoformat() if volunteer.confirmedAt else None,
+        }
+        volunteers_data.append(volunteer_data)
+
+    return jsonify(volunteers_data), 200
