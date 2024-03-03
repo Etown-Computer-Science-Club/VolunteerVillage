9909858d874c13f50bc32d11551806335b635a23
Ethan Lajeunesse <ethan@lajfam.com>
2024-03-02 22:48:43 -0500

    userIsVolunteer

--- api/controllers/posts.py
+++ api/controllers/posts.py
@@ -26,7 +26,8 @@ def get_posts():
                 'city': post.city,
                 'state': post.state,
                 'zip': post.zip,
-            }
+            },
+            'userIsVolunteer': False,
         }
         posts_data.append(post_data)
 
