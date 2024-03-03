b017cbaf22d803d87b829c2a9fbd70dba36dc3da
klinefelters <stevenjk03@gmail.com>
2024-03-02 21:32:25 -0500

    Add CORS support to Flask API and update API URL in client service

--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 from database.db import db
 from dotenv import load_dotenv
 import os
@@ -10,6 +11,8 @@ from controllers.badges import bp as badges_bp
 load_dotenv()
 
 app = Flask(__name__)
+CORS(app, resources={
+     r"*": {"origins": ["http://localhost:3000", "http://localhost:8000"]}})
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("HH_DATABASE_URL")
 app.register_blueprint(posts_bp)
 app.register_blueprint(badges_bp)
--- client/src/services/postService.js
+++ client/src/services/postService.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 const API_URL = process.env.NODE_ENV === 'production'
-	? 'https://api.wordwave.us/post'
-	: 'http://localhost:8000/post';
+	? 'https://api.wordwave.us/posts'
+	: 'http://localhost:8000/posts';
 
 export default class PostService {
 	static async getEvents() {
-		const { data } = await axios.get(API_URL + endpoint)
+		const { data } = await axios.get(API_URL)
+        console.log(data)
 		return (data)
 	}
 }
